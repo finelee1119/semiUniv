708f0b8a9b8693a7d43bc1cc7b0e7b5a0a14205b
fine <finelee1119@gmail.com>
2024-05-03 13:58:30 +0900

    Initial commit

---
+++ src/main/java/com/example/semiUniv/SemiUnivApplication.java
@@ -0,0 +1,13 @@
+package com.example.semiUniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiUnivApplication.class, args);
+	}
+
+}
